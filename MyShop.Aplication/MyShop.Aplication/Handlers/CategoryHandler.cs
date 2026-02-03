afbe936b5c4473e13561b51575d83ddc0bbcb0f6
Meselmias Marques <meselmias.marques.lima@gmail.com>
2026-02-02 21:19:30 -0300

    criacao de paginas e handlers consumindo api

--- MyShop.Aplication/MyShop.Aplication/Handlers/CategoryHandler.cs
+++ MyShop.Aplication/MyShop.Aplication/Handlers/CategoryHandler.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using MyShop.Core.Handlers;
 using MyShop.Core.Model;
 using MyShop.Core.Requests.Categories;
@@ -10,31 +11,40 @@ public class CategoryHandler(IHttpClientFactory httpClientFactory) : ICategoryHa
     private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.ApiName);
 
 
-    public async Task<Response<Category>> Create(EditorCategoryRequest request)
+    public async Task<Response<Category>> CreateAsync(EditorCategoryRequest request)
     {
-        var result = await _client.PostAsJsonAsync("api/Category", request);
+        var result = await _client.PostAsJsonAsync("v1/categories", request);
         
         return await result.Content.ReadFromJsonAsync<Response<Category?>>() 
-            ?? new Response<Category?>(null,400,"Falha ao Criar Categoria");
+               ?? new Response<Category?>(null,400,"Falha ao Criar Categoria");
     }
 
-    public async Task<Response<Category>> Update(EditorCategoryRequest model)
+    public async Task<Response<Category>> UpdateAsync(EditorCategoryRequest model, int id)
     {
-        throw new NotImplementedException();
+        var  result = await _client.PutAsJsonAsync("v1/categories/" + id, model);
+        return await result.Content.ReadFromJsonAsync<Response<Category>>() 
+               ?? new Response<Category>(null,400,"Falha ao Atualizar Categoria");
     }
 
-    public async Task<Response<Category>> Delete(EditorCategoryRequest model)
+    public async Task<Response<Category>> DeleteAsync(EditorCategoryRequest model)
     {
-        throw new NotImplementedException();
+       var result = await _client.DeleteAsync($"v1/categories/{model.Id}");
+       return await result.Content.ReadFromJsonAsync<Response<Category>>() 
+              ?? new Response<Category>(null,400,"Falha ao carregar Categoria");
     }
 
-    public async Task<Response<Category>> GetById(EditorCategoryRequest model)
+    public async Task<Response<Category>> GetByIdAsync(EditorCategoryRequest model)
     {
-        throw new NotImplementedException();
+        var result = await _client.GetAsync($"v1/categories/{model.Id}");
+        return await result.Content.ReadFromJsonAsync<Response<Category>>() 
+               ?? new Response<Category>(null,400,"Falha ao Obter Categoria");
+        
     }
 
     public async Task<Response<List<Category>>> GetAll()
     {
-        throw new NotImplementedException();
+        var result = await _client.GetAsync("v1/categories");
+        return await result.Content.ReadFromJsonAsync<Response<List<Category>>>() 
+               ?? new Response<List<Category>>(null,400,"Falha ao carregar Categoria");
     }
 }
